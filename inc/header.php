a9bc6bfcab02b2e4be914a22b2f32324d0867329
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-16 19:29:27 +0600

    dataBase Connect in config file and database design

--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,13 @@
 <?php
-header("Cache-Control: no-store, no-cache, must-revalidate"); 
-header("Cache-Control: pre-check=0, post-check=0, max-age=0"); 
-header("Pragma: no-cache"); 
-header("Expires: Mon, 6 Dec 1977 00:00:00 GMT"); 
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: pre-check=0, post-check=0, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Mon, 6 Dec 1977 00:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 ?>
 <!doctype html>
 <html>
+
 <head>
 	<title>Online Exam System with PHP OOP jQuery AJAX </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -18,18 +19,18 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 	<script src="js/jquery.js"></script>
 	<script src="js/main.js"></script>
 </head>
+
 <body>
 
-<div class="phpcoding">
-	<section class="headeroption"></section>
+	<div class="phpcoding">
+		<section class="headeroption"></section>
 		<section class="maincontent">
-		<div class="menu">
-		<ul>
-			<li><a href="index.php">Login</a></li>
-			<li><a href="profile.php">Profile</a></li>
-			<li><a href="exam.php">Exam</a></li>
-			<li><a href="register.php">Register</a></li>
-			<li><a href="logout.php">Logout</a></li>
-		</ul>
-		</div>
-	
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Login</a></li>
+					<li><a href="profile.php">Profile</a></li>
+					<li><a href="exam.php">Exam</a></li>
+					<li><a href="register.php">Register</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
