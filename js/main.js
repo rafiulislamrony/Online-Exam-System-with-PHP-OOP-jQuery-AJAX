230fa56ae4dfcd12080b1f1a34c3b4d96f495ee1
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-17 20:37:49 +0600

    Message FadeOut

--- js/main.js
+++ js/main.js
@@ -5,15 +5,13 @@ $(function () {
         var username = $("#username").val();
         var email = $("#email").val();
         var password = $("#password").val();
-
         var dataString = 'name=' + name + '&username=' + username + '&password=' + password + '&email=' + email;
-
         $.ajax({
             type: 'POST',
             url: 'getregister.php',
             data: dataString,
             success: function (data) {
-                $("#state").html(data);
+                $("#state").html(data); 
             }
         });
         return false;
@@ -26,26 +24,27 @@ $(function () {
     $("#loginsubmit").click(function () {
         var email = $("#email").val();
         var password = $("#password").val();
-
         var dataString = '&email=' + email + '&password=' + password;
-
         $.ajax({
             type: 'POST',
             url: 'getlogin.php',
             data: dataString,
             success: function (data) {
                 if ($.trim(data) == "empty") {
-                    $(".empty").show();
-                    $(".disable").hide();
-                    $(".error").hide();
-                } else if ($.trim(data) == "disable") {
-                    $(".empty").hide();
-                    $(".disable").show();
-                    $(".error").hide();
-                } else if ($.trim(data) == "error") {
-                    $(".empty").hide();
-                    $(".disable").hide();
+                    $(".empty").show(); 
+                    setTimeout(function(){
+                        $(".error").fadeOut();
+                    },2000);
+                } else if ($.trim(data) == "disable") { 
+                    $(".disable").show(); 
+                    setTimeout(function(){
+                        $(".error").fadeOut();
+                    },2000);
+                } else if ($.trim(data) == "error") { 
                     $(".error").show();
+                    setTimeout(function(){
+                        $(".error").fadeOut();
+                    },2000);
                 } else {
                     window.location = "exam.php";
                 } 
